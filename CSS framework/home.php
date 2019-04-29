9197b6bbf0eb9057c7c5471b6fac45c7c1ed0290
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2019-04-29 10:22:25 +0200

    |

---
+++ CSS framework/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<div id='header'>
+		<div id='idky'>
+			<button></button>
+		</div>			
+		<h1 id='headertext'>MIJN PORTFOLIO</h1>
+	</div>
+	
+</body>
+</html>
